800c21b5337384a95008ba95aab6ecc443c498f9
Michael Ashton <mykl.ashton@gmail.com>
2017-07-19 14:04:55 -0400

    MVP for daily project

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import EarthquakeInfo from './EarthquakeInfo.js';
+import EarthquakeList from './EarthquakeList.js';
+import axios from 'axios'
 
-class App extends Component {
+export default class App extends Component {
+
   render() {
     return (
       <div className="App">
-
+          <div className="title">
+              <div className="my-header">Earthquakes!</div>
+          </div>
+          <EarthquakeInfo />
+          <EarthquakeList />
       </div>
     );
   }
 }
-
-export default App;
